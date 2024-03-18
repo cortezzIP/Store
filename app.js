c3dc04502a6df69c73868b386cdfc63d18e631e5
CORTEZZ <sorikson083@gmail.com>
2024-03-18 22:48:42 +0500

    micro update

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const fs = require("fs");
 const express = require("express");
-const scripts = require("./js/scripts");
+const { Web3 } = require("web3");
+//const scripts = require("./js/scripts");
+
+const web3 = new Web3("http://localhost:7545");
+
+const buildFile = fs.readFileSync("./src/build/contracts/CStore.json");
+const parsedBuildFile = JSON.parse(buildFile);
+
+const contractAddress = "0x1c587d06b109dA411204c72197b00c2096258903";
+
+const store = new web3.eth.Contract(parsedBuildFile.abi, contractAddress);
+
+web3.eth.defaultAccount = "0x09db776c7B23bcDD4f165a8c1b29197cAAFB688b";
 
 const app = express();
 const urlencodedParser = express.urlencoded({ extended: false });
@@ -8,14 +20,22 @@ const urlencodedParser = express.urlencoded({ extended: false });
 app.use(express.static(__dirname + "/"));
 
 app.post("/", urlencodedParser, function (request, response) {
-  if (!request.body) return response.sendStatus(400);
-  scripts.createProduct(
-    request.body.productId,
-    request.body.productName,
-    request.body.productPrice
-  );
-
-  response.send("ok");
+  //if (!request.body) return response.sendStatus(400);
+  console.log(request.body);
+  var productId = request.body.productId;
+  var productName = request.body.productName;
+  var productPrice = request.body.productPrice;
+  console.log(`${productId} - ${productName} - ${productPrice}`);
+  store.methods
+    .createProduct(Number(productId), productName, Number(productPrice))
+    .send({
+      from: web3.eth.defaultAccount,
+      gas: "300000",
+      gasPrice: 1000000000,
+    })
+    .then(function (data) {
+      res.send(data.transactionHash);
+    });
 });
 
 app.listen(7545, () => {
--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,24 @@ const web3 = new Web3("http://localhost:7545");
 const buildFile = fs.readFileSync("./src/build/contracts/CStore.json");
 const parsedBuildFile = JSON.parse(buildFile);
 
-const contractAddress = "0x66AA853D2bA98333c33db8BCF9e6F455FC4Ea654";
+const contractAddress = "0x1c587d06b109dA411204c72197b00c2096258903";
 
 const store = new web3.eth.Contract(parsedBuildFile.abi, contractAddress);
 
-web3.eth.defaultAccount = "0x6b8B3802d24749572F81a1efDF20F0074aF8cD70";
+web3.eth.defaultAccount = "0x09db776c7B23bcDD4f165a8c1b29197cAAFB688b";
 
 module.exports = {
   createProduct: async function (productId, productName, productPrice) {
     await store.methods
-      .createProduct(productId, productName, productPrice)
-      .send({ from: web3.eth.defaultAccount });
+      .createProduct(Number(productId), productName, Number(productPrice))
+      .send({
+        from: web3.eth.defaultAccount,
+        gas: "300000",
+        gasPrice: 1000000000,
+      })
+      .then(function (data) {
+        res.send(data.transactionHash);
+      });
   },
 
   shipProduct: async function (productId, storeAddress) {
@@ -41,5 +48,5 @@ module.exports = {
     await store.methods
       .addSupplier(supplierAddress, supplierRating)
       .send({ from: web3.eth.defaultAccount });
-  }
+  },
 };
